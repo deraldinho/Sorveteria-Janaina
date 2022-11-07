bc703ae71d80e4660a1383dcfe67ae1e5e11256a
Deraldo Palomino Filho <deraldinhopalomino@hotmail.com>
2022-11-07 17:54:24 -0300

    Atualização do programa

--- sorveteria.py
+++ sorveteria.py
@@ -11,104 +11,127 @@ PR_G = 24.00
     Classe: pedido tem como criar um classe para anotar e controlar os pedidos realizados
     
 '''
+
 class Pedidos:
 
-    def verifi_tamanho(self,tamanho):
-        if (tamanho == 'P' or tamanho == 'p'
-                or tamanho == 'M' or tamanho == 'm'
-                or tamanho == 'G' or tamanho == 'g'):
-            if(tamanho == 'P'):
-                return tamanho
-            elif(tamanho == 'p'):
-                return 'P'
-            if(tamanho == 'M'):
-                return tamanho
-            elif(tamanho == 'm'):
-                return 'M'
-            if(tamanho == 'G'):
-                return tamanho
-            elif(tamanho == 'g'):
-                return 'G'
-        else:
-            print('! ! ! ! !  TAMANHO ou CODIGO INVALIDO(S)')
 
-    def verifi_codigo(self,codigo):
-        if(codigo == 'TR' or codigo == 'tr'
-            or codigo == 'ES' or codigo == 'es'
-            or codigo == 'PR' or codigo == 'pr'):
-            if(codigo == 'TR'):
-                return codigo
-            elif(codigo == 'tr'):
-                return 'TR'
-            if(codigo == 'ES'):
-                return codigo
-            elif(codigo == 'es'):
-                return 'ES'
-            if(codigo == 'PR'):
-                return codigo
-            elif(codigo == 'pr'):
-                return 'PR'
-        else:
-            print('! ! ! ! !  TAMANHO ou CODIGO INVALIDO(S)')
+    def __init__(self):
+        self.codigo = ''
+        self.tamanho = ''
+        self.preco = 0.00
+        self.precoTotal = 0.00
 
-    def set_tamanho(self, tamanho):
-        self.tamanho = self.verifi_tamanho(tamanho)
+    def add_pedido(self, tamanho, codigo):
+        self.tamanho=self.verif_Tamanho(tamanho)
+        self.codigo=self.verif_Codigo(codigo)
+        self.calc_preco()
+        self.calc_precoTotal()
 
-    def set_codigo(self, codigo):
-        self.codigo = self.verifi_codigo(codigo)
-
-    def get_pedido(self):
-        if(self.codigo == 'TR' and self.tamanho == 'P'):
+    def calc_preco(self):
+        aux = self.codigo+'_'+self.tamanho
+        if(aux == 'TR_P'):
             return TR_P
-        if(self.codigo == 'TR' and self.tamanho == 'M'):
+        elif(aux == 'TR_M'):
             return TR_M
-        if (self.codigo == 'TR' and self.tamanho == 'G'):
+        elif(aux == 'TR_G'):
             return TR_G
-        if (self.codigo == 'ES' and self.tamanho == 'P'):
+        elif (aux == 'ES_P'):
             return ES_P
-        if (self.codigo == 'ES' and self.tamanho == 'G'):
+        elif (aux == 'ES_M'):
             return ES_M
-        if (self.codigo == 'PR' and self.tamanho == 'P'):
+        elif (aux == 'ES_G'):
+            return ES_G
+        elif (aux == 'PR_P'):
             return PR_P
-        if (self.codigo == 'PR' and self.tamanho == 'P'):
+        elif (aux == 'PR_M'):
             return PR_M
-        if (self.codigo == 'PR' and self.tamanho == 'P'):
+        elif (aux == 'PR_G'):
             return PR_G
 
-    def get_tamanho(self):
-        return self.tamanho
-    def get_codigo(self):
-        return self.codigo
+    def calc_precoTotal(self):
+        aux = self.codigo + '_' + self.tamanho
+        if(aux == 'TR_P'):
+            self.precoTotal+= TR_P
+        elif(aux == 'TR_M'):
+            self.precoTotal += TR_M
+        elif(aux == 'TR_G'):
+            self.precoTotal += TR_G
+        elif (aux == 'ES_P'):
+            self.precoTotal += ES_P
+        elif (aux == 'ES_M'):
+            self.precoTotal += ES_M
+        elif (aux == 'ES_G'):
+            self.precoTotal += ES_G
+        elif (aux == 'PR_P'):
+            self.precoTotal += PR_P
+        elif (aux == 'PR_M'):
+            self.precoTotal += PR_M
+        elif (aux == 'PR_G'):
+            self.precoTotal += PR_G
 
-class interface(Pedidos):
+    def verif_Tamanho(self, tamanho):
+        if(tamanho == 'P' or tamanho == 'p'):
+            return 'P'
+        elif (tamanho == 'M' or tamanho == 'm'):
+            return 'M'
+        elif (tamanho == 'G' or tamanho == 'g'):
+            return 'P'
+        else:
+            print('Tamanho ou Codigo INVALIDO!!!')
+    def verif_Codigo(self,codigo):
+        if(codigo == 'TR' or codigo == 'tr'):
+            return 'TR'
+        elif (codigo == 'ES' or codigo == 'es'):
+            return 'ES'
+        elif (codigo == 'PR' or codigo == 'pr'):
+            return 'PR'
+        else:
+            pass
 
-    def __int__(self):
-        self.pedido = []
-        self.control="S"
+    def verif_sabor(self):
+        if(self.codigo == 'TR' or self.codigo == 'tr'):
+            return 'Tradicionais'
+        elif (self.codigo == 'ES' or self.codigo == 'es'):
+            return 'Especiais'
+        elif (self.codigo == 'PR' or self.codigo == 'pr'):
+            return 'Premium'
+        else:
+            pass
+
+class Interface:
+
+    control = 'S'
+    count = 0
+    pedido = Pedidos()
+    def loop(self):
+        self.home()
         while(True):
-            print('Bem vindo a Soveteria  Janaina Lessa de Paula')
-            print(
-                '|----------------------------------------------------Cardapio----------------------------------------------------|')
-            print(
-                '|    Código  |   Descrição               |   Tamanho P(500ml)   |    Tamanho M(1000ml)   |   Tamanho G(2000ml)   |')
-            print(
-                '|    TR      |   Sabores Tradicionais    |   R$  6,00           |    R$  10,00           |   R$  18,00           |')
-            print(
-                '|    ES      |   Sabores Especiais       |   R$  7,00           |    R$  12,00           |   R$  21,00           |')
-            print(
-                '|    PR      |   Sabores Premium         |   R$  8,00           |    R$  14,00           |   R$  24,00           |')
-            print(
-                '|----------------------------------------------------------------------------------------------------------------|')
+            if self.control == 'S' or self.control == 's':
+                self.pedido.add_pedido(input('Entre com Tamanho do Pote desejado (P/M/G): '),input('Entre com codigo do sabor desejado (TR/ES/PR): '))
+                print(f'Você Pediu um sorvete sabor {self.pedido.verif_sabor()} de R$ {self.pedido.calc_preco()}')
+                print('------------------------------------------------------------------------------------------')
+                self.control = input('Deseja Pedir mais alguma coisa? (S/N)')
 
-            if self.control== 'S' or self.control== 's':
-                print('qwe')
-            elif self.control== 'N' or self.control== 'n':
-                print('qwe')
+            elif self.control == 'N' or self.control == 'n':
+                print(f'O total a se pago é: R$ {self.pedido.precoTotal}');
+                break
             else:
-                print('qwe')
-
+                pass
+    def home(self):
+        print('Bem vindo a Soveteria  Janaina Lessa de Paula')
+        print(
+            '|----------------------------------------------------Cardapio----------------------------------------------------|')
+        print(
+            '|    Código  |   Descrição               |   Tamanho P(500ml)   |    Tamanho M(1000ml)   |   Tamanho G(2000ml)   |')
+        print(
+            '|    TR      |   Sabores Tradicionais    |   R$  6,00           |    R$  10,00           |   R$  18,00           |')
+        print(
+            '|    ES      |   Sabores Especiais       |   R$  7,00           |    R$  12,00           |   R$  21,00           |')
+        print(
+            '|    PR      |   Sabores Premium         |   R$  8,00           |    R$  14,00           |   R$  24,00           |')
+        print(
+            '|----------------------------------------------------------------------------------------------------------------|')
 
 if __name__ == '__main__':
-    print('sss')
-
-
+    screen = Interface()
+    screen.loop()
