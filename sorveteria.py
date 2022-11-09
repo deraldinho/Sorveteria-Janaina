b57e52747e630946baf3f20e99f8f7a861d43340
Deraldo Palomino Filho <deraldinhopalomino@hotmail.com>
2022-11-09 14:25:03 -0300

    Atualização do programa

--- sorveteria.py
+++ sorveteria.py
@@ -14,11 +14,7 @@ class Pedidos:
     '''
     def __init__(self):
         '''
-        a 'Pedidos.__init__()' ser como estanciamentos para variaveis
-            @:param self.codigo aqui sera armazenado temporariamente codigo do produto
-            @:param self.tamanho aqui sera armazenado temporariamente o tamanho do produto
-            @:param self.preco aqui sera armazenada o valor do produto final
-            @:param self.precototal aqui sera armazenado o valor de todos o produtos calculado enquando o software esta funcionando
+            a 'Pedidos.__init__()' ser como estanciamentos para variaveis
         '''
         self.codigo = ''
         self.tamanho = ''
@@ -26,52 +22,66 @@ class Pedidos:
         self.precoTotal = 0.00
 
     def add_pedido(self, tamanho, codigo):
-        self.tamanho=self.verif_Tamanho(tamanho)
-        self.codigo=self.verif_Codigo(codigo)
-        self.calc_preco()
-        self.calc_precoTotal()
+        '''
+        :param tamanho: tamanho do sorvete
+        :param codigo: codigo do sorte
+        :return: True ou False parametro booleano
+        '''
+        if not self.verif_Tamanho(tamanho):
+            return False
+        elif(not self.verif_Codigo(codigo)):
+            return False
+        else:
+            self.tamanho = tamanho
+            self.codigo = codigo
+            self.calc_preco()
+            self.calc_precoTotal()
+            return True
 
     def calc_preco(self):
         aux = self.codigo+'_'+self.tamanho
-        if(aux == 'TR_P'):
+        if(aux == 'TR_P' or aux == 'tr_p'):
             return TR_P
-        elif(aux == 'TR_M'):
+        elif(aux == 'TR_M' or aux == 'tr_m'):
             return TR_M
-        elif(aux == 'TR_G'):
+        elif(aux == 'TR_G' or aux == 'tr_g'):
             return TR_G
-        elif (aux == 'ES_P'):
+        elif (aux == 'ES_P' or aux == 'es_p'):
             return ES_P
-        elif (aux == 'ES_M'):
+        elif (aux == 'ES_M' or aux == 'es_m'):
             return ES_M
-        elif (aux == 'ES_G'):
+        elif (aux == 'ES_G' or aux == 'es_g'):
             return ES_G
-        elif (aux == 'PR_P'):
+        elif (aux == 'PR_P' or aux == 'pr_p'):
             return PR_P
-        elif (aux == 'PR_M'):
+        elif (aux == 'PR_M' or aux == 'pr_m'):
             return PR_M
-        elif (aux == 'PR_G'):
+        elif (aux == 'PR_G' or aux == 'pr_g'):
             return PR_G
-
+        else:
+            return False
     def calc_precoTotal(self):
         aux = self.codigo + '_' + self.tamanho
-        if(aux == 'TR_P'):
+        if(aux == 'TR_P' or aux == 'tr_p'):
             self.precoTotal+= TR_P
-        elif(aux == 'TR_M'):
+        elif (aux == 'TR_M' or aux == 'tr_m'):
             self.precoTotal += TR_M
-        elif(aux == 'TR_G'):
+        elif (aux == 'TR_G' or aux == 'tr_g'):
             self.precoTotal += TR_G
-        elif (aux == 'ES_P'):
+        elif (aux == 'ES_P' or aux == 'es_p'):
             self.precoTotal += ES_P
-        elif (aux == 'ES_M'):
+        elif (aux == 'ES_M' or aux == 'es_m'):
             self.precoTotal += ES_M
-        elif (aux == 'ES_G'):
+        elif (aux == 'ES_G' or aux == 'es_g'):
             self.precoTotal += ES_G
-        elif (aux == 'PR_P'):
+        elif (aux == 'PR_P' or aux == 'pr_p'):
             self.precoTotal += PR_P
-        elif (aux == 'PR_M'):
+        elif (aux == 'PR_M' or aux == 'pr_m'):
             self.precoTotal += PR_M
-        elif (aux == 'PR_G'):
+        elif (aux == 'PR_G' or aux == 'pr_g'):
             self.precoTotal += PR_G
+        else:
+            return False
 
     def verif_Tamanho(self, tamanho):
         if(tamanho == 'P' or tamanho == 'p'):
@@ -81,7 +91,7 @@ class Pedidos:
         elif (tamanho == 'G' or tamanho == 'g'):
             return 'P'
         else:
-            print('Tamanho ou Codigo INVALIDO!!!')
+            return False
     def verif_Codigo(self,codigo):
         if(codigo == 'TR' or codigo == 'tr'):
             return 'TR'
@@ -90,7 +100,7 @@ class Pedidos:
         elif (codigo == 'PR' or codigo == 'pr'):
             return 'PR'
         else:
-            print('Tamanho ou Codigo INVALIDO!!!')
+            return False
 
     def verif_sabor(self):
         if(self.codigo == 'TR' or self.codigo == 'tr'):
@@ -100,7 +110,7 @@ class Pedidos:
         elif (self.codigo == 'PR' or self.codigo == 'pr'):
             return 'Premium'
         else:
-            pass
+            return False
 
 class Interface:
 
@@ -111,16 +121,15 @@ class Interface:
         self.home()
         while(True):
             if self.control == 'S' or self.control == 's':
-                self.pedido.add_pedido(input('Entre com Tamanho do Pote desejado (P/M/G): '),input('Entre com codigo do sabor desejado (TR/ES/PR): '))
-                print(f'Você Pediu um sorvete sabor {self.pedido.verif_sabor()} de R$ {self.pedido.calc_preco()}')
+                if not self.pedido.add_pedido(input('Entre com Tamanho do Pote desejado (P/M/G): '),input('Entre com codigo do sabor desejado (TR/ES/PR): ')):
+                    print('Tamanho ou Codigo INVALIDO!!!')
+                else:
+                    print(f'Você Pediu um sorvete sabor {self.pedido.verif_sabor()} de R$ {self.pedido.calc_preco()}')
                 print('------------------------------------------------------------------------------------------')
-                self.control = input('Deseja Pedir mais alguma coisa? (S/N)')
-
             elif self.control == 'N' or self.control == 'n':
                 print(f'O total a se pago é: R$ {self.pedido.precoTotal}');
                 break
-            else:
-                pass
+            self.control = input('Deseja Pedir mais alguma coisa? (S/N)')
     def home(self):
         print('Bem vindo a Soveteria  Janaina Lessa de Paula')
         print(
